ad3565cdba5c6b0889705ea05c71cf3d85ec7f50
liloudemierre <161479296+liloudemierre@users.noreply.github.com>
2025-11-19 10:12:50 +0100

    animation1

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+// Séquence 1
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".bird",
+    start: "top top",
+    end: "bottom+=800% top",
+    pin: true,
+    scrub: 1,
+    markers: false,
+  },
+});
+
+tl.to(".bird-image", { x: "350%", y: "-250%", duration: 6, ease: "none" })
+  .to(".beach-image", { y: "-95%", duration: 6, ease: "none" }, "+=5")
+  .to(".beach-text", { ease: "sine.inOut", opacity: "100", duration: 7 }, "+=2")
+  .to(".beach-image", { scale: "20", x: "720%", duration: 7, delay: "10" })
+  .to(".beach-text", { x: "1000%", duration: 7, delay: "-7" })
+  .to(".bird", { opacity: "0" });
+
+// Séquence 2
+const tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".tourists",
+    start: "top top",
+    end: "bottom+=500% top",
+    pin: true,
+    scrub: true,
+    markers: false,
+  },
+});
+
+tl2
+  .to(".tourists-text", { ease: "sine.inOut", opacity: "100", duration: "3" })
+  .to(".grass-right", { x: "-70%", duration: "3" })
+  .to(".grass-left", { x: "70%", duration: "3" }, "-=3")
+  .to(".tourists-right", { x: "-70%", duration: "3" }, "-=3")
+  .to(".tourists-left", { x: "70%", duration: "3" }, "-=3")
+  .to(".tourists-text", { duration: 2, ease: "sine.inOut", opacity: "0" })
+  .to(".grass-right, .vacancier-right", { x: "70%", duration: "1" })
+  .to(".grass-left, .vacancier-left", { x: "-70%" }, "-=1");
